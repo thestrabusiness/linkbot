02dc639cb95863dc19e53190e4cf251043f6f41e
Anthony M <anthony@followapp.me>
2017-05-22 19:30:42 -0400

    cleanup routes.rb, remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace :api do
     post 'registration/' => 'registration#register', as: :register
   end
 
   resources :links
   resources :dashboard, only: :index
-  resources :registration, only: :new
 
   root to: 'dashboard#index'
 end
