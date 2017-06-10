7fcd390b3b2773f5373b98067be56f2430ffb0f9
Anthony M <anthony@followapp.me>
2017-06-09 20:54:50 -0400

    add pundit, add link policy, filter links by user team, authorize actions on link by user team

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
+  include Pundit
+
   protect_from_forgery with: :exception
+
+  def authenticate_user
+    redirect_to new_session_path unless current_user.present?
+  end
 end
---
+++ app/policies/link_policy.rb
@@ -0,0 +1,35 @@
+class LinkPolicy
+  attr_reader :user, :link
+
+  def initialize(user, link)
+    @user = user
+    @link = link
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.joins(user_from: :team).where('users.team_id': user.team.id)
+    end
+  end
+
+  def show?
+    link_belongs_to_users_team?
+  end
+
+  def destroy?
+    link_belongs_to_users_team?
+  end
+
+  private
+
+  def link_belongs_to_users_team?
+    user.team == link.team
+  end
+end
