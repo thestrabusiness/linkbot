f6754c5571d7d035bd85e9c3f35d3c699c5141ba
Anthony M <anthony@followapp.me>
2017-06-16 20:53:50 -0400

    add placeholder for link image and link domain

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def link_image(link)
+    if link.metadata.present? && link.metadata.image.present?
+      image_tag(link.metadata.image.expiring_url, class: 'link-image')
+    else
+      inline_svg('placeholder.svg', class: 'link-image')
+    end
+  end
+
+  def link_domain(link)
+    link.domain || link.url
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,11 +1,12 @@
 class Link < ActiveRecord::Base
+  include InlineSvg
+
   belongs_to :user_from, class_name: 'User'
   has_one :team, through: :user_from
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :tagged_users, class_name: 'User', join_table: 'user_tags'
   belongs_to :metadata
 
-  delegate :image, to: :metadata, allow_nil: true
   delegate :title, to: :metadata, allow_nil: true
   delegate :domain, to: :metadata, allow_nil: true
   delegate :description, to: :metadata, allow_nil: true
